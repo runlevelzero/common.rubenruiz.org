94c994ca2f6fd6b4d0c39d782a608d2ab79b3167
Ruben <runlevelzero@users.noreply.github.com>
2019-04-04 15:51:11 -0500

    Update commonheader.js

--- js/commonheader.js
+++ js/commonheader.js
@@ -1,10 +1,13 @@
 // This file was made by Ruben Ruiz 
 function makeHeader() {
   var header =  $("<div></div>").attr("class","header");
-  var link1 = $("<a></a>").attr({
+  var headertext     = $("<h1></h1>").attr("class", "header");
+  $(headertext).text("Ruben Ruiz");
+  var link1  = $("<a></a>").attr({
     "href" : "https://git.rubenruiz.org",
     "class" : "header"
   }).text("git repos");
   $("body").prepend(header);
+  $("div.header").append(headertext);
   $("div.header").append(link1);
 }
