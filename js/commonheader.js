30592e7f2b53396975ee2b095ccbc26224b30f82
Ruben <runlevelzero@users.noreply.github.com>
2019-04-05 13:47:11 -0500

    cleaned it up a bit

--- js/commonheader.js
+++ js/commonheader.js
@@ -1,20 +1,28 @@
 // This file was made by Ruben Ruiz 
 function makeHeader() {
-  var header =  $("<div></div>").attr("class","header");
-  var headertext     = $("<h1></h1>").attr("class", "header");
-  $(headertext).text("Ruben Ruiz");
-  var link1  = $("<a></a>").attr({
+  // Create the elements
+  var header     =  $("<div></div>").attr("class","header");
+  var headertext = $("<h1></h1>").attr("class", "header");
+  var link1      = $("<a></a>").attr({
     "href" : "https://git.rubenruiz.org",
     "class" : "header"
   });
-  var link2  = $("<a></a>").attr({
+  var link2      = $("<a></a>").attr({
     "href" : "https://pass.rubenruiz.org",
     "class" : "header"
   });
+  
+  // Assign Text Values
+  headertext.text("Ruben Ruiz");
   link1.text("git repos");
   link2.text("password manager");
+  
+  // Add the elements to the header
+  header.append(headertext);
+  header.append(link1);
+  header.append(link2);
+  
+  // Put it in the Body
   $("body").prepend(header);
-  $("div.header").append(headertext);
-  $("div.header").append(link1);
-  $("div.header").append(link2);
+  
 }
